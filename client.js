25899b22b2b204bb630fc74aef7cce50e560a0eb
michaelbromander <michaelbromander@gmail.com>
2020-03-07 14:44:09 -0600

    basic style, basic html and js/jq setup

---
+++ client.js
@@ -0,0 +1,27 @@
+console.log( 'js' );
+$(document).ready( readyNow );
+
+function readyNow(){
+    console.log( 'JQ' );
+
+}
+
+// the application should have: employee first name, last name, ID, title, annual salary.
+// A "Submit" button should collect the form information to calculate monthly cost, append information to 
+// the DOM and clear the input fields. Using the stored information, calculate monthly costs and append this to the DOM.
+//  If total monthly costs exceeds &20,000, add a red background color to the total monthly cost.
+//  Create a delete button that removes an employee from the DOM. For base mode, it does not need to remove that Employee's
+//  salary from the reported total.
+
+
+let employeeFile = [];
+
+
+
+
+
+
+
+
+
+
